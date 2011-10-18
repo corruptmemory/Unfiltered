6b0c54a38a794dcf5d8354189afffcf4510f2fc0
Jim Powers <jim@corruptmemory.com>
2011-10-18 16:55:26 -0400

    Some cleanup and added coalesce

--- scalaz/src/main/scala/monad/package.scala
+++ scalaz/src/main/scala/monad/package.scala
@@ -2,4 +2,15 @@ package unfiltered
 import scalaz._
 import Scalaz._
 
-package object monad extends RequestLoggers
+package object monad extends RequestLoggers {
+  def coalesce(args:Seq[String],split:Option[String => Seq[String]] = none):Option[Seq[String]] =
+    (args.foldLeft(Vector[String]()) {
+      (b:Vector[String],v:String) => v.trim match {
+        case "" => b
+        case s0 => (split map ((f:String => Seq[String]) => b ++ (f(s0) map (_.trim) filterNot (!_.isEmpty)))) | (b :+ (s0))
+      }
+    }) match {
+      case Vector() => none
+      case vs => some(vs)
+    }
+}
